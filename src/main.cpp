589052b7c19dfccc098f525998b999309ca97269
Tauno Erik <sedumacre@gmail.com>
2022-07-16 18:34:57 +0300

    test commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
 
+#define RADAR_PIN 2
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  // Boot message
+  Serial.print("Project: ");
+  Serial.println("\"Kolm Silma\"");
+  Serial.print("Compiled: ");
+  Serial.print(__TIME__);
+  Serial.print(" ");
+  Serial.println(__DATE__);
+  Serial.println("Made by Tauno Erik.");
+
+  pinMode(RADAR_PIN, INPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool is_movement = digitalRead(RADAR_PIN);
+
+  if (is_movement) {
+    Serial.print("H");
+  } else {
+    Serial.print("L");
+  }
 }
